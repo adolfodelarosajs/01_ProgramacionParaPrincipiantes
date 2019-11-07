5eab15046054b517b073563846b42662f119bb2c
adolfodelarosajs <57353220+adolfodelarosajs@users.noreply.github.com>
2019-11-07 22:34:51 +0100

    Add files via upload

---
+++ Seccion03/FahrenheitCelsius.js
@@ -0,0 +1,12 @@
+/*
+Convertir de grados Fahrenheit a Celsius
+La formula para hacer la conversión es la siguiente:
+    C = ( F - 32 ) * ( 5 / 9 )   
+*/
+
+let celcius;
+let fahrenheit = 90;
+
+celcius = (fahrenheit - 32 ) * ( 5 / 9);
+
+console.log(fahrenheit + " grados fahrenheit equivalen a " + celcius + " grados celcius.");
